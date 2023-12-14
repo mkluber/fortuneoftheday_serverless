4f7cb1611e7743729335976d063089f3cf7aa880
Mark Kluber <145130822+mkluber@users.noreply.github.com>
2023-12-14 18:06:12 +0100

    .

--- static/script.js
+++ static/script.js
@@ -1,7 +1,6 @@
 function myFunction() {
     console.log('Hello world!');
-  }
-  
+}
 myFunction();
 
 function ScanFortune() {
@@ -105,7 +104,7 @@ function ReadFortune() {
   fetch(url, {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/json",
     }
   })
     .then(response => response.json())
@@ -116,23 +115,165 @@ function ReadFortune() {
 }
 
 function UpdateFortune() {
-  // A <form> element
-  const FortuneInfo = document.querySelector("#UpdateFortuneForm");
-  const formData = new FormData(FortuneInfo);
-  const response = fetch("https://api.outworldindustries.com/updatefortune", {
-    method: "POST",
-    body: formData,
+
+  //Get the form element by id
+  const sampleForm = document.getElementById("UpdateFortuneForm");
+
+  //Add an event listener to the form element and handler for the submit an event.
+  sampleForm.addEventListener("submit", async (e) => {
+    /**
+     * Prevent the default browser behaviour of submitting
+     * the form so that you can handle this instead.
+     */
+    e.preventDefault();
+
+    /**
+     * Get the element attached to the event handler.
+     */
+    let form = e.currentTarget;
+
+    /**
+     * Take the URL from the form's `action` attribute.
+     */
+    let url = 'https://api.outworldindustries.com/updatefortune';
+
+    try {
+      /**
+       * Takes all the form fields and make the field values
+       * available through a `FormData` instance.
+       */
+      let formData = new FormData(form);
+
+      /**
+       * The `postFormFieldsAsJson()` function in the next step.
+       */
+      let responseData = await postFormFieldsAsJson({ url, formData });
+
+      //Destructure the response data
+      let { serverDataResponse } = responseData;
+
+      //Display the response data in the console (for debugging)
+      console.log(serverDataResponse);
+    } catch (error) {
+      //If an error occurs display it in the console (for debugging)
+      console.error(error);
+    }
   });
-  console.log(response);
+
+  /**
+   * Helper function to POST data as JSON with Fetch.
+   */
+  async function postFormFieldsAsJson({ url, formData }) {
+    //Create an object from the form data entries
+    let formDataObject = Object.fromEntries(formData.entries());
+    // Format the plain form data as JSON
+    let formDataJsonString = JSON.stringify(formDataObject);
+
+    //Set the fetch options (headers, body)
+    let fetchOptions = {
+      //HTTP method set to POST.
+      method: "POST",
+      //Set the headers that specify you're sending a JSON body request and accepting JSON response
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      // POST request body as JSON string.
+      body: formDataJsonString,
+    };
+
+    //Get the response body as JSON.
+    //If the response was not OK, throw an error.
+    let res = await fetch(url, fetchOptions);
+
+    //If the response is not ok throw an error (for debugging)
+    if (!res.ok) {
+      let error = await res.text();
+      throw new Error(error);
+    }
+    //If the response was OK, return the response body.
+    return res.json();
+  }
 }
 
 function DeleteFortune() {
-  // A <form> element
-  const FortuneInfo = document.querySelector("#DeleteFortuneForm");
-  const formData = new FormData(FortuneInfo);
-  const response = fetch("https://api.outworldindustries.com/deletefortune", {
-    method: "POST",
-    body: formData,
+
+  //Get the form element by id
+  const sampleForm = document.getElementById("DeleteFortuneForm");
+
+  //Add an event listener to the form element and handler for the submit an event.
+  sampleForm.addEventListener("submit", async (e) => {
+    /**
+     * Prevent the default browser behaviour of submitting
+     * the form so that you can handle this instead.
+     */
+    e.preventDefault();
+
+    /**
+     * Get the element attached to the event handler.
+     */
+    let form = e.currentTarget;
+
+    /**
+     * Take the URL from the form's `action` attribute.
+     */
+    let url = 'https://api.outworldindustries.com/deletefortune';
+
+    try {
+      /**
+       * Takes all the form fields and make the field values
+       * available through a `FormData` instance.
+       */
+      let formData = new FormData(form);
+
+      /**
+       * The `postFormFieldsAsJson()` function in the next step.
+       */
+      let responseData = await postFormFieldsAsJson({ url, formData });
+
+      //Destructure the response data
+      let { serverDataResponse } = responseData;
+
+      //Display the response data in the console (for debugging)
+      console.log(serverDataResponse);
+    } catch (error) {
+      //If an error occurs display it in the console (for debugging)
+      console.error(error);
+    }
   });
-  console.log(response);
+
+  /**
+   * Helper function to POST data as JSON with Fetch.
+   */
+  async function postFormFieldsAsJson({ url, formData }) {
+    //Create an object from the form data entries
+    let formDataObject = Object.fromEntries(formData.entries());
+    // Format the plain form data as JSON
+    let formDataJsonString = JSON.stringify(formDataObject);
+
+    //Set the fetch options (headers, body)
+    let fetchOptions = {
+      //HTTP method set to POST.
+      method: "POST",
+      //Set the headers that specify you're sending a JSON body request and accepting JSON response
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      // POST request body as JSON string.
+      body: formDataJsonString,
+    };
+
+    //Get the response body as JSON.
+    //If the response was not OK, throw an error.
+    let res = await fetch(url, fetchOptions);
+
+    //If the response is not ok throw an error (for debugging)
+    if (!res.ok) {
+      let error = await res.text();
+      throw new Error(error);
+    }
+    //If the response was OK, return the response body.
+    return res.json();
+  }
 }
